9c101fffe51a3c69a42acbfdb8506536480671f8
mingsam <784427618@qq.com>
2023-06-09 14:14:46 +0800

    init git & program framework init

---
+++ update_config.py
@@ -0,0 +1,23 @@
+import os
+import requests
+import logging
+import docker
+
+class MyClash():
+    def __init__():
+        pass
+
+    def _init_logs_utils():
+        pass
+
+    def get_new_config():
+        pass
+    
+    def merge_configs():
+        pass
+    
+    def refresh_clash_docker():
+        pass
+
+    def check_clash():
+        pass
